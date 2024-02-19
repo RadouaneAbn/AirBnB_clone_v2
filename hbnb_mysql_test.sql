2d060da4a5b4be986bfd0890ccd878c942336f8b
aelbouaa <hbouaami@gmail.com>
2024-02-19 14:08:00 -0800

    creating database hbnb_dev_db and user hbnb_dev creating database hbnb_test_db and user hbnb_test

---
+++ hbnb_mysql_test.sql
@@ -0,0 +1,7 @@
+-- script that prepares a MySQL for the project
+-- create database hbnb_test_db and user hbnb_test in localhost with pwd hbnb_test_pwd
+-- Grant all  priviliges on hbnb_test and selecton on performace_schema
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* To hbnb_test@localhost;
