b7602f468076ce240d06ee9b60accc256ea2333f
aelbouaa <hbouaami@gmail.com>
2024-02-19 17:25:07 -0800

    adding updated files for task 7 and 8

--- models/city.py
+++ models/city.py
@@ -5,5 +5,7 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullbale=False)
+    name = Column(String(128), nullbale=False)
+    Places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
