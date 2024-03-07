d56aa26ee530af44a630395e37e82da41453d784
RadouaneAbn <radouaneabn.com>
2024-03-07 21:28:14 +0100

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This script contains a function that
+"""
+from fabric.api import *
+import os
+from datetime import datetime
+
+
+def do_pack():
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = f"web_static_{current_time}.tgz"
+    try:
+        local(f"tar -cvzf versions/{file_name} web_static")
+        return f"versions/{file_name}"
+    except Exception:
+        return None
