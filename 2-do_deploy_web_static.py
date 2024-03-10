0bc60fa35d6afb6575f38910e28d37c9e6a6f3be
RadouaneAbn <radouaneabn.com>
2024-03-10 18:28:28 +0000

    update do_deploy function7

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,69 +3,43 @@
 """
 from fabric.api import *
 import os
-from datetime import datetime
+import time
 import re
 
 env.hosts = ['52.3.252.27', '54.160.117.45']
 env.user = "ubuntu"
+import time
 
 
 def do_pack():
-    """ This function generates a .tgz archive from contents of the
+    """ This script generates a .tgz archive from contents of the
         web_static folder
     """
-    if not os.path.exists("versions"):
-        os.mkdir("versions")
-    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = f"web_static_{current_time}.tgz"
+    local("mkdir -p versions")
+    archive = "versions/web_static_{}.tgz".format(
+            time.strftime("%Y%m%d%H%M%S"))
     try:
-        local(f"tar -cvzf versions/{file_name} web_static")
-        return f"versions/{file_name}"
+        local("tar -cvzf {} web_static".format(archive))
+        return archive
     except Exception:
         return None
 
 
 def do_deploy(archive_path):
+    """ This function destributes an archive to my web servers
     """
-        Deploys and distributes an archive to web servers
-    """
+    if not os.path.exists(archive_path):
+        return False
 
-    if os.path.exists(archive_path):
-        put(archive_path, "/tmp/")
-        archive_file = archive_path[9:]
-        server_archive = "/tmp/{}".format(archive_file)
+    file_name = re.search(r"([^\/]*)\..*$", archive_path)[1]
+    releases_path = f"/data/web_static/releases/{file_name}"
 
-        archive_base, ext = os.path.splitext(archive_file)
-        new_path = "/data/web_static/releases/{}".format(archive_base)
-
-        run("sudo mkdir -p {}".format(new_path))
-        run("sudo tar -zxf {} -C {}".format(server_archive, new_path))
-        run("sudo rm  {}".format(server_archive))
-        run("sudo mv {}/web_static/* {}".format(new_path, new_path))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_path))
-
-        print("New version deployed!")
-
-        return True
-
-    return False
-
-# def do_deploy(archive_path):
-#     """ This function destributes an archive to my web servers
-#     """
-#     if not os.path.exists(archive_path):
-#         return False
-
-#     file_name = re.search(r"([^\/]*)\..*$", archive_path)[1]
-#     releases_path = f"/data/web_static/releases/{file_name}"
-
-#     put(archive_path, "/tmp/")
-#     run("sudo mkdir -p {}".format(releases_path))
-# run("sudo tar -zxf /tmp/{}.tgz -C {}".format(file_name, releases_path))
-# run("sudo cp -rf {}/web_static/* {}".format(releases_path, releases_path))
-#     run("sudo rm -r {}/web_static/".format(releases_path))
-#     run("sudo rm -rf /data/web_static/current")
-#     run("sudo ln -s {} /data/web_static/current".format(releases_path))
-#     print("New version deployed!")
-#     return True
+    put(archive_path, "/tmp/")
+    run("sudo mkdir -p {}".format(releases_path))
+    run("sudo tar -zxf /tmp/{}.tgz -C {}".format(file_name, releases_path))
+    run("sudo cp -rf {}/web_static/* {}".format(releases_path, releases_path))
+    run("sudo rm -r {}/web_static".format(releases_path))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(releases_path))
+    print("New version deployed!")
+    return True
