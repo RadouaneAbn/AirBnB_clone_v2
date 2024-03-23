df6d423e28a598ec53646a6d835c880979bbc1b4
RadouaneAbn <radouaneabn.com>
2024-03-23 19:05:33 +0000

    pycodestyle

--- models/state.py
+++ models/state.py
@@ -2,16 +2,8 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, MetaData, Table, DateTime
-
+from sqlalchemy import Column, String
 from os import environ
-
-# metadata = MetaData()
-# states = Table('places', metadata,
-#                Column(String(60), unique=True, nullable=False, primary_key=True),
-#                Column(DateTime, default=datetime.now(), nullable=False)
-#             Column(DateTime, default=datetime.now(), nullable=False)
-# )
 
 
 class State(BaseModel, Base):
@@ -20,7 +12,6 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref='state',
                           cascade="all, delete-orphan")
-
 
     if not environ.get('HBNB_TYPE_STORAGE') == "db":
         @property
