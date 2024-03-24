60cf590e6790a7e110079820a5b7a6a0402bb505
RadouaneAbn <redouane1997.ocs@gmail.com>
2024-03-24 22:25:31 +0000

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """
     This model starts a Flask web application
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 
@@ -21,13 +21,67 @@ def teardown_db(exception=None):
     storage.close()
 
 
+@app.route("/", strict_slashes=False)
+def home():
+    """Route to display 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route to display 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def greet_c(text):
+    """Route to display 'C ' followed by string"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def greet_python(text="is cool"):
+    """Route to display 'python ' followed by a string"""
+    return "python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def is_number(n):
+    """Route to display '<n> is a number' if n is a number"""
+    if n.isdigit():
+        return "{} is a Number".format(n)
+    else:
+        abort(404)
+
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def display_number(n):
+    """Route to display number using a template."""
+    if n.isdigit():
+        return render_template("5-number.html", number=n)
+    else:
+        abort(404)
+
+
+@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+def display_number_type(n):
+    """Route to display if the number is odd or even using a template"""
+    if n.isdigit():
+        type_n = "even" if int(n) % 2 == 0 else "odd"
+        return render_template("6-number_odd_or_even.html",
+                               number=n, type_n=type_n)
+    else:
+        abort(404)
+
+
 @app.route("/states_list")
 def states_list():
     """ This function displays an HTML page """
     state_insts = get_db(State).values()
     state_insts = sorted(state_insts, key=lambda state: state.name)
     return render_template("7-states_list.html",
-                           list_state_inst=state_insts)
+                           states=state_insts)
 
 
 if __name__ == "__main__":
